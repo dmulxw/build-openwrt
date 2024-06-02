0404781560b2afcf78422cae2fa5381ac1569ff2
lixiaowei <267199@qq.com>
2024-06-02 22:27:58 +0800

    mt7621

--- config/openwrt-main/diy-part1.sh
+++ config/openwrt-main/diy-part1.sh
@@ -12,3 +12,18 @@
 # other
 # rm -rf package/utils/{ucode,fbtest}
 
+
+sudo apt-get update
+sudo apt-get install -y libpcre3 libpcre3-dev
+
+# Add a feed source
+#echo 'src-git openwrtpackages https://github.com/openwrt/packages' >>feeds.conf.default
+
+# 生成feeds.conf.default
+cat <<EOF > feeds.conf.default
+#src-git packages https://github.com/openwrt/openwrt.git
+src-git packages https://github.com/openwrt/packages.git
+src-git luci https://github.com/openwrt/luci.git
+src-git routing https://github.com/openwrt-routing/packages.git
+src-git telephony https://github.com/openwrt/telephony.git
+EOF
